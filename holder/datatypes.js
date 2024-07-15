66417bde16298ae31409e8229d904377ffe571e2
MrSanjayKadam <kadamasanjay361@gmail.com>
2024-07-15 13:18:44 +0000

    datatype commit

---
+++ holder/datatypes.js
@@ -0,0 +1,22 @@
+// premmitive
+
+//  1.number
+//  2.BigInt
+//  3.String
+//  4.boolean
+//  5.null
+//  6.undefined
+//  7.style
+
+// non primitive
+
+// 1.object
+// 2.
+
+let score = "66abc";
+
+let newscore=Number(score)
+console.log(typeof (newscore))
+
+console.log(typeof(score))
+console.log(newscore)
