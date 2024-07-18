70a00100cc0a0158cbe5745d72e689d9f8b6ba42
MrSanjayKadam <kadamasanjay361@gmail.com>
2024-07-18 16:02:31 +0000

    delete

---
+++ holder/array.js
@@ -0,0 +1,10 @@
+let myarray =new Array(1,2,3,4,5,6,7,8,9,);
+
+// console.log(myarray.push("brain"))
+// console.log(myarray.pop())
+// console.log(myarray.shift())
+console.log(myarray.shift())
+
+console.log(myarray)
+
+
