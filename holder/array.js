082ed9697c85ab93a3918134434bbc182e897ed6
MrSanjayKadam <kadamasanjay361@gmail.com>
2024-07-19 19:30:25 +0000

    array revisiom complete

--- holder/array.js
+++ holder/array.js
@@ -1,10 +1,3 @@
-let myarray =new Array(1,2,3,4,5,6,7,8,9,);
+let 
 
-// console.log(myarray.push("brain"))
-// console.log(myarray.pop())
-// console.log(myarray.shift())
-console.log(myarray.shift())
 
-console.log(myarray)
-
-hb
